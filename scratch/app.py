85735e9e3ae69f33075f62bdf2879402a1d69ffc
Katie McLaughlin <katie@glasnt.com>
2020-07-12 11:34:43 +1000

    init proof

--- scratch/app.py
+++ scratch/app.py
@@ -3,6 +3,14 @@ import httpx
 import json
 import textwrap 
 from github import Github
+import emoji
+
+from datetime import datetime
+
+starttime = datetime.now()
+
+def remove_emoji(text):
+    return emoji.get_emoji_regexp().sub(u'', text)
 
 # Automatically provided by GitHub Actions. For local testing, provide your own. 
 # https://docs.github.com/en/actions/configuring-and-managing-workflows/authenticating-with-the-github_token#about-the-github_token-secret
@@ -28,11 +36,12 @@ def sidebyside(a, b):
     return "\n".join(res)
 
 
-def short(n, w=32, p=" .."):
+def short(n, w=32, p=""):
     return textwrap.shorten(n, width=w, placeholder=p)
 
 def table(s, w=40, l="|", r="|", t="_", b="_"): 
     ll = w - len(l) - len(r) 
+    s = remove_emoji(s)
     lli = ll - 2
     res = []
     if t != "":
@@ -44,7 +53,7 @@ def table(s, w=40, l="|", r="|", t="_", b="_"):
             nline = line.ljust(lli)
         res.append(f"{l} {nline} {r}")
     if b != "": 
-        res.append(f"{l}{b * ll}{r}")
+        res.append(f"{l}{b * ll}{r}\n")
     return "\n".join(res)
 
 def flattable(s, w=40):
@@ -76,19 +85,13 @@ avatar_art = """
   .. .(%%%%%%%&%..**.,,,....&&& 
    . #%&%&%&&%&&&..,.......&&&  
      #&&%&&%&%&&&&%......&&#    
-        (&&%%&&&&&&&&&&&        
+        (&&%%&&&&&&&&&&&
 """
-avatar = flattable(avatar_art)
-print(avatar_art)
-print(avatar)
 
 userq = """query
 {
   user(login: "%s") {
     followers {
-      totalCount
-    }
-    following {
       totalCount
     }
     starredRepositories {
@@ -101,15 +104,14 @@ userq = """query
 resp = graphql(userq)
 data = json.loads(resp)["data"]["user"]
 followers = data["followers"]["totalCount"]
-following = data["following"]["totalCount"]
 starred = data["starredRepositories"]["totalCount"]
 
-userblock = avatar + flattable(f"""
+avatar = flattable(avatar_art, w=36)
+userblock = avatar +"\n" + flattable(f"""
 {user.name}
 {user.login}
-{user.bio}
-¤ {followers} followers · {following} following · ✭ {starred} 
-
+{short(user.bio, w=36)}
+¤ {followers} followers · ✭ {starred} 
 """, w=36)
 
 
@@ -150,28 +152,28 @@ for node in data["data"]["user"]["pinnedItems"]["edges"]:
     if "nameWithOwner" in n.keys(): 
         pinned_block = textwrap.dedent(f"""
         [] {short(n["nameWithOwner"], p='')}
-        {short(n["description"])}
+        {short(remove_emoji(n["description"]).strip())}
 
-        {n["primaryLanguage"]["name"]} ✭ {n["stargazers"]["totalCount"]} ↡ {n["forks"]["totalCount"]}
-        """)
+        {n["primaryLanguage"]["name"]} ✭ {n["stargazers"]["totalCount"]} ↡ {n["forks"]["totalCount"]}""")
     else:
         pinned_block = textwrap.dedent(f"""
         <> {n["description"]}
 
 
-
         """)
     
-    res = table(pinned_block)
-    pinned.append(res)
+    pinned.append(pinned_block)
 
 
 # hacks
 
-pinnedheader = " Pinned                                                   Customize your pins\n"
-pinnedblock = pinnedheader + sidebyside(pinned[0], pinned[1]) + sidebyside(pinned[2], pinned[3]) + sidebyside(pinned[4], pinned[5])
+pinnedheader = " Pinned                                                     Customize your pins\n"
+pinnedblock = (pinnedheader + sidebyside(table(pinned[0]), table(pinned[1])) + 
+              sidebyside(table(pinned[2], t=""), table(pinned[3], t="")) + 
+              sidebyside(table(pinned[4], t=""), table(pinned[5], t="")))
 
 final = sidebyside(userblock, pinnedblock)
-print(final)
 
+delta = datetime.now() - starttime
 
+print(f"```\n{final}\n```\n<!--- generated in {delta} -->")
